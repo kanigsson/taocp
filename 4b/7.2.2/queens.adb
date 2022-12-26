5c9b7cea26656d08d49333d8a3db079adfbd1308
Johannes Kanig <kanig@adacore.com>
2022-12-26 10:45:43 +0900

    ex 7

--- 4b/7.2.2/queens.adb
+++ 4b/7.2.2/queens.adb
@@ -1,7 +1,6 @@
 with Ada.Text_IO;
 procedure Queens is
-	procedure Run (R : Integer) is
-		N : constant := 8;
+	procedure Run (N : Integer) is
 		A : array (1 .. N) of Boolean;
 		X : array (1 .. N) of Integer;
 		B, C : array (1 .. 2 * N - 1) of Boolean;
@@ -10,7 +9,9 @@ procedure Queens is
 		Count : Integer := 0;
 		procedure Visit is
 		begin
-			Count := Count + 1;
+			if X (1) = 2 and then X (2) = N and then X (N - 1) = 1 and then X (N) = N - 1 then
+				Count := Count + 1;
+			end if;
 		end Visit;
 	begin
 		<<B1>>
@@ -24,10 +25,7 @@ procedure Queens is
 		end if;
 
 		<< B3>>
-		if L = R then
-			L := L + 1;
-			goto B2;
-		elsif A (T) or else B (T + L -1) or else C (T - L + N) then
+		if A (T) or else B (T + L -1) or else C (T - L + N) then
 			goto B4;
 		else
 			A (T) := True; B (T + L -1) := True; C (T - L + N) := True; X (L) := T; L:= L + 1;
@@ -42,9 +40,6 @@ procedure Queens is
 
 		<<B5>>
 		L := L - 1;
-		if L = R then
-			L := L - 1;
-		end if;
 		if L > 0 then
 			T := X (L); C (T - L + N) := False; B (T + L - 1) := False; A (T) := False;
 			goto B4;
@@ -52,7 +47,7 @@ procedure Queens is
 	  Ada.Text_IO.Put_Line (Integer'image (Count));
 	end Run;
 begin
-	for I in 1 .. 8 loop
+	for I in 5 .. 17 loop
 		Run (I);
 	end loop;
 end Queens;
